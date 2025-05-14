c34e44c6e3ff76f2c9d376eae81812b2b70efeda
Bijan Alvandi <bijan.alvandi@gmail.com>
2025-05-14 10:15:15 +0330

    Initial commit

---
+++ Directory_Tree_Printer.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+user_directory = input("Enter the path of the directory: ")
+
+directory_list = []
+file_list = []
+directory_dict = {}
+
+try:
+    for dirpath, dirnames, filenames in os.walk(user_directory):ca
+        print(dirnames)
+        # directory_list.append(dirnames)
+        # file_list.append(filenames)
+        directory_dict[tuple(dirnames)] = tuple(filenames)
+    # print(directory_list)
+    # print(file_list)
+    # print(directory_dict)
+
+
+except FileNotFoundError:
+    print("Directory Not Found")
+    sys.exit(1)
+except PermissionError:
+    print("Permission Denied")
+    sys.exit(1)
